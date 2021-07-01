d576553b3b9dcb5d09e995dadc3c3ef642b1b70d
akashaggarwal <aggarwal_akasha@yahoo.com>
2021-07-01 17:16:42 -0400

    Update Settings.cs

--- Config/Settings.cs
+++ Config/Settings.cs
@@ -7,7 +7,7 @@ namespace Config
         //ToDo: Enter a valid Service Bus connection string
         // Copy from ServiceBus->SharedAccesPolicies->RootManageSharedAccesseKey->PrimaryConnectionString unless you created seperate key for 
         //each subscriber
-        public static string ConnectionString = "Endpoint=sb://cmsivr.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kgoAHT7xnYGuZqK28rSIb20rHJ24Nixzfn8QhSSeQrQ=";
+        public static string ConnectionString = "Endpoint=sb://.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=\=";
         public static string TopicName = "cms";
 
         public static int threads = 1;
